88af4f57db140d889d093450656211753af5d371
kwingert3 <speedy_d_w@yahoo.com>
2021-05-14 19:02:44 -0400

    Updated Pet Clinic Data pom for newer maven version config. SfgPetClinicApplicationTests was not in the correct package. springframework instead of spring.framework.

--- pet-clinic-web/src/test/java/guru/spring/framework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/spring/framework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework.sfgpetclinic;
+package guru.spring.framework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
