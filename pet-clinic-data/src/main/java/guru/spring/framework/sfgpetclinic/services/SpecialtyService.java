cb13128b6d484475504b0783032b113eb16b60d4
kwingert3 <speedy_d_w@yahoo.com>
2021-05-16 01:03:06 -0400

    Added SpecialityMapService. Closes #32

---
+++ pet-clinic-data/src/main/java/guru/spring/framework/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,9 @@
+package guru.spring.framework.sfgpetclinic.services;
+
+
+import guru.spring.framework.sfgpetclinic.model.Speciality;
+
+
+
+public interface SpecialtyService extends CrudService<Speciality,Long>{
+}
---
+++ pet-clinic-data/src/main/java/guru/spring/framework/sfgpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,36 @@
+package guru.spring.framework.sfgpetclinic.services.map;
+
+import guru.spring.framework.sfgpetclinic.model.Speciality;
+import guru.spring.framework.sfgpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public class SpecialityMapService extends AbstractMapService<Speciality,Long> implements SpecialtyService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findByID(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
